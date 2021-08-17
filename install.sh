8e0d148ea18043ca0239b3d040049e249ef2e5eb
YioChou <yiochou1227@gmail.com>
2021-08-17 23:19:41 +0800

    Format comments

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #!/bin/zsh
 
-# --- tmux -------------------------------
+# === tmux ==============================
 ln -s -f $PWD/.tmux.conf ~/.tmux.conf
-tmux source-file ~/.tmux.*
 
-# --- bash_prompt --------------------------------
+# === bash_prompt =======================
 echo "source $PWD/.bash_prompt" >> ~/.bash_prompt;
 
-# --- bashrc --------------------------------
+# === bashrc ============================
 echo "source $PWD/.bashrc" >> ~/.bashrc;
 
-# --- bash_profile (for Mac Terminal) --------------------------------
+# === gitconfig =========================
+echo "[include] \n\t path = $PWD/.gitconfig" >> ~/.gitconfig;
+
+# === bash_profile (for Mac Terminal) ===
 # https://joshstaiger.org/archives/2005/07/bash_profile_vs.html
 # echo "source $PWD/.bash_profile" >> ~/.bash_profile;
-
-
-# --- git ------------------------------------------------
-echo "[include] \n\t path = $PWD/.gitconfig" >> ~/.gitconfig;
